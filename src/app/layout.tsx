12668f452f75c7d3f243d0f5868da3cbe96d8c16
Yogesh <yogeshn4120@gmail.com>
2026-05-11 12:34:54 +0530

    added target

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
     <html lang="en">
       <body>{children}
       <Script
-          src="https://assets.adobedtm.com/4fa03d1212c6/aff5f8797720/launch-068785ad451b-development.min.js"
+          src="https://assets.adobedtm.com/4fa03d1212c6/1bcc82a1f2a7/launch-dcd67db7e624-development.min.js"
           strategy="afterInteractive"
         />
       </body>
