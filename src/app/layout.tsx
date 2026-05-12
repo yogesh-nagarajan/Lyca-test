53a2cc8fa1536cc2b29826f603559ca4687d5b68
Yogesh <yogeshn4120@gmail.com>
2026-05-12 15:22:39 +0530

    changed target

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
-import Script from 'next/script';
-
-import './globals.css';
+import Script from "next/script";
+import "./globals.css";
 
 export default function RootLayout({
   children,
@@ -9,15 +8,36 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}
-      <Script
+      <head>
+
+        {/* Pre-hiding snippet */}
+        <Script id="adobe-prehide" strategy="beforeInteractive">
+          {`
+            (function(d){
+              var style = d.createElement('style');
+              style.id = 'at-body-style';
+              style.innerHTML = 'body{opacity:0!important}';
+              d.head.appendChild(style);
+
+              setTimeout(function(){
+                var el = d.getElementById('at-body-style');
+                if(el) el.remove();
+              }, 3000);
+            })(document);
+          `}
+        </Script>
+
+        {/* Adobe Launch */}
+        <Script
           src="https://assets.adobedtm.com/4fa03d1212c6/1bcc82a1f2a7/launch-92730bc39d65-development.min.js"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
         />
-      
+
+      </head>
+
+      <body>
+        {children}
       </body>
     </html>
   );
 }
-
-
